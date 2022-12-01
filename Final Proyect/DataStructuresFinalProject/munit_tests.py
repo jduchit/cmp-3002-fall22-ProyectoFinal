4a0bd4b065114da28b9d8c5454f997e409c257f7
Johana Duchi Tipán <112076346+jduchit@users.noreply.github.com>
2022-12-01 05:34:03 -0500

    Update munit_tests.py
    Add test Fibonacci and recursion

--- Final Proyect/DataStructuresFinalProject/munit_tests.py
+++ Final Proyect/DataStructuresFinalProject/munit_tests.py
@@ -1,4 +1,5 @@
-from DataStructures import Array, Doubly_Linked_List, Singly_Linked_List, Queue, Priority_Queue, Stacks, FactorialRecursion,FactorialMemoization
+from DataStructures import Array, Doubly_Linked_List, Singly_Linked_List, Queue, Priority_Queue, Stacks, FactorialRecursion,FactorialMemoization, 
+FibonacciRecursion,FibonacciMemoization
 
 from FactorialRecursion import factorial
 from FactorialMemoization import factorial
@@ -6,6 +7,7 @@ from FactorialMemoization import factorial
 import unittest
 import hypothesis.strategies as st
 from hypothesis import given
+ import math
 
 
 class TestFactorial(unittest.TestCase):  
@@ -48,6 +50,53 @@ class TestFactorial(unittest.TestCase):
     def test_n_greater_than_0(self,n):
         result = int(factorial(n) / factorial(n - 1))
         self.assertEqual(n, result)
+        
+
+class TestFibonacci(unittest.TestCase):  
+    """
+    Check that the fibonacci function raises a "TypeError" if the n entered is 
+    a letter
+    """
+    def test_n_is_letter(self):
+        with self.assertRaises(TypeError):
+            fibonacci('one')
+    
+    """
+    Check that the fibonacci function raises a "TypeError" if the n entered is 
+    a float number
+    """
+    def test_n_is_float(self):
+        with self.assertRaises(TypeError):
+            fibonacci(1.1)
+            
+    """
+    Check that the fibonacci function raises a "ValueError" if the n entered 
+    is n<0
+    """
+    def test_n_less_than_0(self):
+        with self.assertRaises(ValueError):
+            fibonacci(-1)
+            
+    """
+    Check that the result of the fibonacci is '0' if 'n == 0' 
+    """
+    def test_n_is_0(self):
+        self.assertEqual(fibonacci(0), 0)
+
+    """
+    Check that the result of the fibonacci is '1' if 'n == 1' 
+    """
+    def test_n_is_1(self):
+        self.assertEqual(fibonacci(1), 1)
+
+    """
+    Check that the fibonacci function was calculated correctly by using the Binet’s Formula this is a formula that is 
+    used to find the nth term of the Fibonacci sequence
+    """
+    @given(st.integers(min_value=2, max_value=20))
+    def test_n_greater_than_1(self,n):
+        result = int(((((1 + math.sqrt(5)) / 2) ** (n))-(((1 - math.sqrt(5)) / 2) ** (n)))/(math.sqrt(5)))
+        self.assertEqual(fibonacci(n), result)
 
 if __name__ == '__main__':
     unittest.main()
