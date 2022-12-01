fb1771a51f61da4c6c560a77bfd38df3f46ea8f3
Johana Duchi Tipán <112076346+jduchit@users.noreply.github.com>
2022-12-01 05:08:25 -0500

    Update munit_tests.py

--- Final Proyect/DataStructuresFinalProject/munit_tests.py
+++ Final Proyect/DataStructuresFinalProject/munit_tests.py
@@ -1,1 +1,53 @@
-from DataStructures import Array, Doubly_Linked_List, Singly_Linked_List, Queue, Priority_Queue, Stacks
+from DataStructures import Array, Doubly_Linked_List, Singly_Linked_List, Queue, Priority_Queue, Stacks, FactorialRecursion,FactorialMemoization
+
+from FactorialRecursion import factorial
+from FactorialMemoization import factorial
+
+import unittest
+import hypothesis.strategies as st
+from hypothesis import given
+
+
+class TestFactorial(unittest.TestCase):  
+    """
+    Check that the factorial function raises a "TypeError" if the n entered is 
+    a letter
+    """
+    def test_n_is_letter(self):
+        with self.assertRaises(TypeError):
+            factorial('one')
+    
+    """
+    Check that the factorial function raises a "TypeError" if the n entered is 
+    a float number
+    """
+    def test_n_is_float(self):
+        with self.assertRaises(TypeError):
+            factorial(1.1)
+            
+    """
+    Check that the factorial function raises a "ValueError" if the n entered 
+    is n<0
+    """
+    def test_n_less_than_0(self):
+        with self.assertRaises(ValueError):
+            factorial(-1)
+            
+    """
+    Check that the result of the factorial is '1' if 'n == 0' 
+    """
+    def test_n_is_0(self):
+        self.assertEqual(factorial(0), 1)
+
+    """
+    Check that the factorial function was calculated correctly by using the 
+    definition that says the factorial of a number divided by the factorial of 
+    that number minus one is the original number
+    """
+    @given(st.integers(min_value=2, max_value=100))
+    def test_n_greater_than_0(self,n):
+        result = int(factorial(n) / factorial(n - 1))
+        self.assertEqual(n, result)
+
+if __name__ == '__main__':
+    unittest.main()
